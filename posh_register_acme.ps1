50d789648f98040f05e9e1d85d6db19aef5024ec
Scott0Bailey <37932761+scott0bailey@users.noreply.github.com>
2026-03-04 20:15:29 -0500

    Install Posh-ACME and PANOShell modules
    Added installation commands for Posh-ACME and PANOShell modules.

--- posh_register_acme.ps1
+++ posh_register_acme.ps1
@@ -1,3 +1,9 @@
+# Needs to be run once time pwer server
+# Install posh acme
+Install-Module -Name Posh-ACME.Deploy -Scope CurrentUser
+Install-Module PANOShell -scope CurrentUser
+
+# Register acme account
 $eabKID = '<eab>'
 $eabHMAC = '<hmac>'
 $email = '<email>'
