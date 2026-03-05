c16adf7b9910dd134f5926453743a2f54355ecc4
Scott0Bailey <37932761+scott0bailey@users.noreply.github.com>
2026-03-04 21:10:18 -0500

    Refactor acme.sh script for better structure and clarity

--- linux_acme_request.sh
+++ linux_acme_request.sh
@@ -1,22 +1,43 @@
-## STANDALONE:
-export CN=<CN>
-export LOC=/path/to/certificates
-acme.sh --issue --nginx -d $CN --fullchain-file $LOC/fullchain.pem --key-file $LOC/privkey.pem -k 2048
+## STANDALONE
+export CN="<CN>"
+export LOC="/path/to/certificates"
+mkdir -p "${LOC}"
+acme.sh --issue \
+  --nginx \
+  -d "${CN}" \
+  --fullchain-file "${LOC}/fullchain.pem" \
+  --key-file "${LOC}/privkey.pem" \
+  -k 2048
 
 
-## MD
-export CN=<CN>
-export SAN1=<SAN1>
-export SAN2=<SAN2>
-export LOC=/path/to/certificates
-acme.sh --issue --nginx -d $CN -d $SAN1 -d $SAN2 --fullchain-file $LOC/fullchain.pem --key-file $LOC/privkey.pem -k 2048
+## MULTI-DOMAIN
+export CN="<CN>"
+export SAN1="<SAN1>"
+export SAN2="<SAN2>"
+export LOC="/path/to/certificates"
+mkdir -p "${LOC}"
+acme.sh --issue \
+  --nginx \
+  -d "${CN}" \
+  -d "${SAN1}" \
+  -d "${SAN2}" \
+  --fullchain-file "${LOC}/fullchain.pem" \
+  --key-file "${LOC}/privkey.pem" \
+  -k 2048
 
 
-## With Post hooks
-export CN=<CN>
-export SAN1=<SAN1>
-export SAN2=<SAN2>
-export RESTART_CMD=<restart_cmd>
-acme.sh --issue --nginx -d $CN -d $SAN1 -d $SAN2 --post-hook "cat /root/.acme.sh/$CN/$CN.cer /root/.acme.sh/$CN/$CN.key > /home/tls/tls.pem && $RESTART_CMD -k 2048
+## WITH POST-HOOK
+export CN="<CN>"
+export SAN1="<SAN1>"
+export SAN2="<SAN2>"
+export RESTART_CMD="<restart_cmd>"
+export TLS_OUT="/home/tls/tls.pem"
+acme.sh --issue \
+  --nginx \
+  -d "${CN}" \
+  -d "${SAN1}" \
+  -d "${SAN2}" \
+  --post-hook "cat \"${HOME}/.acme.sh/${CN}/${CN}.cer\" \"${HOME}/.acme.sh/${CN}/${CN}.key\" > \"${TLS_OUT}\" && ${RESTART_CMD}" \
+  -k 2048
 
 
