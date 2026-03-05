61b50d241f6ccf0bfbc5c8c5917ec753949ba5e3
Scott0Bailey <37932761+scott0bailey@users.noreply.github.com>
2026-03-04 21:11:21 -0500

    Update linux_register_acme.sh

--- linux_register_acme.sh
+++ linux_register_acme.sh
@@ -1,10 +1,17 @@
 ## Install ACME.sh
-## Getting started with acme.sh client
-##  https://github.com/acmesh-official/acme.sh
-## Recommend always using "sudo -i" to run all acme.sh commands
+## Getting started with acme.sh client:
+##   https://github.com/acmesh-official/acme.sh
+## Recommended: run under sudo -i OR as a dedicated acme user.
 
-## Register ACME Account:
-export EAB_KID=<eab>
-export EAB_HMAC_KEY=<hmac>
-export SECTIGO_ACME=<sectigo_acme_url>
-acme.sh --register-account --insecure --force --eab-kid $EAB_KID --eab-hmac-key $EAB_HMAC_KEY --server $SECTIGO_ACME && acme.sh --set-default-ca --server $SECTIGO_ACME
+## Environment variables for EAB and Sectigo ACME endpoint
+export EAB_KID="<eab_kid>"
+export EAB_HMAC_KEY="<eab_hmac_key>"
+export SECTIGO_ACME="<sectigo_acme_directory_url>"
+## Register ACME Account with EAB
+acme.sh --register-account \
+  --server "${SECTIGO_ACME}" \
+  --eab-kid "${EAB_KID}" \
+  --eab-hmac-key "${EAB_HMAC_KEY}" \
+  --force
+## Set Sectigo as the default CA
+acme.sh --set-default-ca --server "${SECTIGO_ACME}"
